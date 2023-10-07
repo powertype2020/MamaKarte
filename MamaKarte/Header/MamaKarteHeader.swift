26e8d37cafc4be4958a92f638e6fac6b13040583
naoki.takehisa <naoki.takehisa@init-inc.com>
2023-10-07 15:29:50 +0900

    ママかるてリニューアル初期コミット

---
+++ MamaKarte/Header/MamaKarteHeader.swift
@@ -0,0 +1,20 @@
+//
+//  MamaKarteHeader.swift
+//  MamaKarte
+//
+//  Created by 武久 なおき on 2023/10/07.
+//
+
+import SwiftUI
+
+struct MamaKarteHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MamaKarteHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        MamaKarteHeader()
+    }
+}
