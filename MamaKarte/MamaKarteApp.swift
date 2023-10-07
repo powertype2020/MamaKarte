611495827604fc86012ab9724da4c1efb60e81aa
naoki.takehisa <naoki.takehisa@init-inc.com>
2023-10-07 15:16:11 +0900

    Initial Commit

---
+++ MamaKarte/MamaKarteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MamaKarteApp.swift
+//  MamaKarte
+//
+//  Created by 武久 なおき on 2023/10/07.
+//
+
+import SwiftUI
+
+@main
+struct MamaKarteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
